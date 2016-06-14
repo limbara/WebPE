2e3f50eb5d79990751d98a2d0133d12674d1b54d
Nico Limbara <limbaranico@yahoo.com>
2016-06-14 20:06:48 +0700

    create controller in handler/registerPage.js

---
+++ handlers/registerPage.js
@@ -0,0 +1,46 @@
+var registerPage,
+    USER = require('../utils/USER'),
+    COLLECTION = require('../utils/COLLECTION');
+var path = require('path');
+
+var page = function(req,res){
+    res.render('register.html');
+}
+
+var saveUSER = function(req,res,next){
+    // Get the User data from the Form
+    var User={
+        username: req.body.nama,
+        email: req.body.email,
+        password: req.body.password
+    };
+    // Insert the New User to database
+    USER.saveUSER(User).then(function(){
+       USER.findUSER(User.username).then(function(row){
+           // Create First Collection Data
+            var Collection ={
+                photos : JSON.stringify([]),
+                path : path.join(__dirname, '..','public/images/',(row[0].username).toString()),
+                id_user : row[0].id_user
+            }
+            // Insert the New Collection to database
+            COLLECTION.saveCOLLECTION(Collection).then(function(){
+                var dir = Collection.path;
+                // Create User folder named 'username' into public/images/username
+                if (!fs.existsSync(dir)){
+                     fs.mkdirSync(dir);
+                }
+                res.render('info.html',{info: User.username + ' telah terdaftar',back:'/'}); 
+            })
+        })
+        
+    });
+    
+}
+
+registerPage = {
+    page : page,
+    saveUser : saveUSER
+}
+
+module.exports = registerPage;
