5dad3e9cf882f0d93725fcda6ca2e590fcaeb09b
Henry <henzdeimon11@gmail.com>
2016-06-15 23:37:00 +0700

    added favicon for the website

--- middleware.js
+++ middleware.js
@@ -3,11 +3,12 @@ var router = require('./router'),
 	middleware;
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 var express = require('express');
 var path = require('path');
 
 middleware = function(app){
-	
+	app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 	app.use(logger('dev'));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
