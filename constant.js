ed0d5d63273891de1cdf8b4e0d91f14157a69c6f
Henry Kurniawan <henzdeimon11@gmail.com>
2016-06-26 22:40:25 +0700

    added Regex_Username and Regex_Password

--- constant.js
+++ constant.js
@@ -1,4 +1,9 @@
 module.exports = {
 	SERVER_PORT : 3000,
 	JWT_USER_STRING : "UserSecret",
+	// Username must start with Uppercase & Lowercase letter , Cannot contains symbols, Min 6 characters & Max 25 characters
+	REGEX_USERNAME : /^([A-Z]|[a-z])(\w){5,24}$/, 
+	// Password should contains at least 1 digit, Min 8 characters & Max 40 characters
+	REGEX_PASSWORD : /^(?=.*\d)[0-9a-zA-Z]{8,40}$/
+
 }
