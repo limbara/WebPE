59001c3b243d9ddfe7aed56f96d206c991f8c27f
Dedy Suhendra <dedysuhendra@rocketmail.com>
2016-06-27 23:54:51 +0700

    changed User password length to 100

--- model/migrations/20160512215010_User.js
+++ model/migrations/20160512215010_User.js
@@ -2,7 +2,7 @@ var table = function(table){
 	table.increments('id_user').primary();
 	table.string('username',30).notNullable();
 	table.string('email',50).notNullable();
-	table.string('password',40).notNullable();
+	table.string('password',100).notNullable();
 };
 
 
