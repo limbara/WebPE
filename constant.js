fe26557cc3335f0801f96bae2d90b554584dda02
Yunas Hanifah <yunashanifah18@yahoo.com>
2016-06-27 23:49:45 +0700

    added admin secret key

--- constant.js
+++ constant.js
@@ -1,6 +1,7 @@
 module.exports = {
 	SERVER_PORT : 3000,
 	JWT_USER_STRING : "UserSecret",
+	JWT_ADMIN_STRING : "AdminSecret",
 	// Username must start with Uppercase & Lowercase letter , Cannot contains symbols, Min 6 characters & Max 25 characters
 	REGEX_USERNAME : /^([A-Z]|[a-z])(\w){5,24}$/, 
 	// Password should contains at least 1 digit, Min 8 characters & Max 40 characters
