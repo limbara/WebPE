3c414f54650c3b959535456cf1383958397ee1dd
Yunas Hanifah <yunashanifah18@yahoo.com>
2016-06-28 17:14:06 +0700

    added method deleteCOLLECTION

--- utils/COLLECTION.js
+++ utils/COLLECTION.js
@@ -8,17 +8,25 @@ var saveCOLLECTION = function(Collection){
 		});
 };
 
+var updateCOLLECTION = function(photos,path,id_user,query,binding){
+	return knex('collection').update({'photos': photos,'path':path,'id_user':id_user}).whereRaw(query, binding)
+;
+}
+
 var fetchCOLLECTION = function(id_user){
 	return knex('collection').select('photos', 'path').where('id_user', id_user);
 }
 
-var updateCOLLECTION = function(photos,path,id_user,query,binding){
-	return knex('collection').update({'photos': photos,'path':path,'id_user':id_user}).whereRaw(query, binding);
-}
 
+var deleteCOLLECTION = function(id_user){
+	return knex('collection')
+	.where('id_user',id_user)
+	.del();
+};
 
 module.exports = {
 	saveCOLLECTION: saveCOLLECTION,
+	updateCOLLECTION : updateCOLLECTION,
 	fetchCOLLECTION : fetchCOLLECTION,
-	updateCOLLECTION : updateCOLLECTION
+	deleteCOLLECTION : deleteCOLLECTION
 };
